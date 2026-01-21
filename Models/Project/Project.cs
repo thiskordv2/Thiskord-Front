5e6e49f0204222b85c0eaa98c0583d87c8dd9529
hikme <hikmetemreak@gmail.com>
2026-01-21 14:13:20 +0100

    Model Project

---
+++ Models/Project/Project.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Thiskord_Front.Models.Project
+{
+    public class Project
+    {
+        [JsonPropertyName("project_id")]
+        public int? id { get; set; }
+        [JsonPropertyName("project_name")]
+        public string? name { get; set; }
+        [JsonPropertyName("project_desc")]
+        public string? description { get; set; }
+        public Project()
+        {
+        }
+
+        public Project(int id, string name, string description)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+
+        public class ProjectRequest
+        {
+            [JsonPropertyName("project_name")]
+            public string name { get; set; }
+
+            [JsonPropertyName("project_desc")]
+            public string description { get; set; }
+        }
+    }
+
+
+}
+
