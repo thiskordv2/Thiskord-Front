eccd33b4a707e5fe9483979498ac9d1bd3789831
FisaroJuju <68395401+FisaroJuju@users.noreply.github.com>
2026-04-01 19:02:15 +0200

    Modif de SessionService et des ViewModels de Login et Inscription

--- Services/SessionService.cs
+++ Services/SessionService.cs
@@ -9,18 +9,21 @@ namespace Thiskord_Front.Services
         public static SessionService Instance => _instance.Value;
         public string CurrentUser { get; private set; } = string.Empty;
         public string Token { get; private set; } = string.Empty;
+        public UserAccount? CurrentUserAccount { get; private set; }
         public bool IsLoggedIn => !string.IsNullOrEmpty(Token);
 
-        public void Login(string user, string token)
+        public void Login(UserAccount user, string token)
         {
-            CurrentUser = user;
+            CurrentUserAccount = user;
+            CurrentUser = user.user_name ?? string.Empty;
             Token = token;
         }
 
         public void Logout()
         {
-            CurrentUser = "";
-            Token = "";
+            CurrentUser = string.Empty;
+            Token = string.Empty;
+            CurrentUserAccount = null;
         }
 
     }
